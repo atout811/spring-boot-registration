cb45e1720cce88ab0e5a4a068ca34d386cc79e0b
atout811 <dmcgfx811@gmail.com>
2022-09-02 07:25:53 +0200

    registration package

--- src/main/java/com/auth/demo/appuser/AppUserService.java
+++ src/main/java/com/auth/demo/appuser/AppUserService.java
@@ -10,11 +10,11 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class AppUserService implements UserDetailsService {
 
-    private final static String USER_NOT_FOUND_MESSAGE = "user with email %s not found";
+    private final static String USER_NOT_FOUND = "user with email %s not found";
     private final AppUserRepository appUserRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return appUserRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MESSAGE,email)));
+        return appUserRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException(String.format(USER_NOT_FOUND,email)));
     }
 }
---
+++ src/main/java/com/auth/demo/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.auth.demo.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "it works!";
+    }
+}
