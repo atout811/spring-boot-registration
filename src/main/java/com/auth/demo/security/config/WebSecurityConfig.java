feba73876da55ce15941cb80d19b644db84a71db
atout811 <dmcgfx811@gmail.com>
2022-09-02 09:11:41 +0200

    fix bug

--- src/main/java/com/auth/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/auth/demo/security/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider())
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
 
     @Bean
